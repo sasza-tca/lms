018dbfe0e81cbd7d0505d7ae9950000eec5ea5b5
kulus <root@new-host.ig-zp.pl>
2015-02-15 14:18:44 +0100

    add missing )

--- modules/invoice_pdf.inc.php
+++ modules/invoice_pdf.inc.php
@@ -390,8 +390,8 @@ function invoice_data($x, $y, $width, $font_size, $margin)
 					$t_data[$v++] = sprintf('%.2f %%', $item['pdiscount']);
 				elseif (!empty($item['vdiscount']))
 					$t_data[$v++] = iconv("UTF-8", "ISO-8859-2//TRANSLIT", moneyf($item['vdiscount']));
-				elseif (!empty($invoice['pdiscount']) || !empty($invoice['vdiscount'])
-				    $t_data[$v++] = '';
+				elseif (!empty($invoice['pdiscount']) || !empty($invoice['vdiscount']))
+				$t_data[$v++] = '';
 				$t_data[$v++] = iconv("UTF-8", "ISO-8859-2//TRANSLIT", moneyf($item['basevalue']));
 				$t_data[$v++] = iconv("UTF-8", "ISO-8859-2//TRANSLIT", moneyf($item['totalbase']));
 				$t_data[$v++] = iconv("UTF-8", "ISO-8859-2//TRANSLIT", $item['taxlabel']);
